4f34838977ce444d51f057d537be5b451151d701
lokeshchoudharyprogrammer <lokeshchoudhary2026@gmail.com>
2023-07-29 18:52:08 +0530

    Image  Generate Ai

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const readline = require("readline");
 const { generateImage } = require('./controls/img');
 var cors = require('cors')
 const app = express();
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 
 app.use(express.json())
 app.use(express.static('public'))
